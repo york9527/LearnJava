513a473d3f7a5626673c7be66526aa29b01c7c5e
余正文 <3322024289@qq.com>
2018-12-18 10:41:56 +0800

    LinkedList

--- src/com/coderyu/Main.java
+++ src/com/coderyu/Main.java
@@ -1,8 +1,19 @@
 package com.coderyu;
 
+import java.util.LinkedList;
+import java.util.List;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        LinkedList<String> list=new LinkedList<>();
+        list.add("A");
+        list.add("B");
+        list.add("C");
+        list.addFirst("X");
+
+        for (String str : list) {
+            System.out.println(str);
+        }
     }
 }
