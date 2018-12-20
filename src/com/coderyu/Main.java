d492d846bedc05048799d71e78056dedcab32997
余正文 <3322024289@qq.com>
2018-12-20 10:15:12 +0800

    Collections.shuffle test

--- src/com/coderyu/Main.java
+++ src/com/coderyu/Main.java
@@ -1,19 +1,33 @@
 package com.coderyu;
-
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.LinkedList;
 import java.util.List;
 
 public class Main {
-
     public static void main(String[] args) {
-        LinkedList<String> list=new LinkedList<>();
-        list.add("A");
-        list.add("B");
-        list.add("C");
-        list.addFirst("X");
-
-        for (String str : list) {
-            System.out.println(str);
+        String[] fruits=new String[]{"apple","banana","oreng","grape","pineapple"};
+        List<String> arrayList=new ArrayList<>();
+        List<String> linkedList=new LinkedList<>();
+        for(String fruit : fruits){
+            arrayList.add(fruit);
+            linkedList.add(fruit);
         }
+
+        System.out.println(arrayList);
+        System.out.println(linkedList);
+
+        Collections.shuffle(arrayList);
+        Collections.shuffle(linkedList);
+
+        System.out.println("After Collectons.shuffle()");
+        System.out.println(arrayList);
+        System.out.println(linkedList);
     }
-}
+}/* out put
+[apple, banana, oreng, grape, pineapple]
+[apple, banana, oreng, grape, pineapple]
+After Collectons.shuffle()
+[banana, grape, pineapple, apple, oreng]
+[oreng, pineapple, grape, apple, banana]
+*/
